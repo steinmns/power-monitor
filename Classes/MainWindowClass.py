c14250f9df01898210b0dacc1df843a64be14f5f
Nick S <nickasteinmetz@gmail.com>
2020-01-04 13:57:25 -0500

    Added main window and home graph skeleton

---
+++ Classes/MainWindowClass.py
@@ -0,0 +1,32 @@
+#PyQT Dependencies
+from PyQt5 import QtWidgets, QtCore, uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QVBoxLayout
+
+#Graphing Dependencies
+import matplotlib
+matplotlib.use('QT5Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.backends.backend_qt5agg import FigureCanvas
+from matplotlib.figure import Figure
+
+class Main_Win(QMainWindow):
+    
+    def __init__(self):
+        #Constructor Method
+        super(Main_Win, self).__init__()
+        self.ui = uic.loadUi('UI Files/MainWindowUI.ui', self)   #Loads Main Menu Window
+
+    def testGraph(self):
+        fig, ax = plt.subplots()
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        counts = [10,3,4,7,2,12,11,7,3,4,1,5]
+        ax.plot(months, counts)
+        ax.set(xlabel='Month', ylabel='Test Unit', title='Test Plot')
+        ax.grid()
+        plt.xticks(months, rotation='vertical') #FIX the labels going off the edges
+
+        self.plotWidget = FigureCanvas(fig)
+        lay = QtWidgets.QVBoxLayout(self.HomeGraph)  
+        lay.setContentsMargins(0, 0, 0, 0)      
+        lay.addWidget(self.plotWidget)
