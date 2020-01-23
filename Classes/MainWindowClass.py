8fd6ae52c6701d781795cf2dc81de4e999d636d7
Nick S <nickasteinmetz@gmail.com>
2020-01-23 15:42:15 -0500

    Revised Main Window Design

--- Classes/MainWindowClass.py
+++ Classes/MainWindowClass.py
@@ -10,12 +10,36 @@ import numpy as np
 from matplotlib.backends.backend_qt5agg import FigureCanvas
 from matplotlib.figure import Figure
 
+#Icon and Styling Dependencies
+import qtawesome as qta #Possibly make this only material icons at some point
+
+#Database Dependencies
+import mysql.connector
+
 class Main_Win(QMainWindow):
     
     def __init__(self):
         #Constructor Method
         super(Main_Win, self).__init__()
         self.ui = uic.loadUi('UI Files/MainWindowUI.ui', self)   #Loads Main Menu Window
+
+        #Tab Icons
+        hometab_icon = qta.icon('mdi.home-outline')
+        insighttab_icon = qta.icon('mdi.chart-line')
+        self.CentralTabWidget.setTabIcon(0, hometab_icon)
+        self.CentralTabWidget.setTabIcon(1, insighttab_icon)
+
+        #Settings Button Setup
+        settings_icon = qta.icon('mdi.settings-outline')
+        self.SettingsButton = self.findChild(QtWidgets.QPushButton, 'SettingsButton') 
+        self.SettingsButton.setIcon(settings_icon)
+        #self.SettingsButton.clicked.connect(self.displaySettingsMenu)  #WHEN SETTINGS UI FILES ARE ADDED FINISH THIS
+
+        #Help Button Setup
+        help_icon = qta.icon('mdi.help-circle-outline')
+        self.HelpButton = self.findChild(QtWidgets.QPushButton, 'HelpButton')
+        self.HelpButton.setIcon(help_icon)
+        #self.HelpButton.clicked.connect(self.displayHelpWindow)    #WHEN HELP MENU UI FILES ARE ADDED FINISH THIS
 
     def testGraph(self):
         fig, ax = plt.subplots()
