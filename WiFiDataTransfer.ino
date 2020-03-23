f43b7ba5714a6a05667a08d55b1b6d519c077bcb
wickstae <60888796+wickstae@users.noreply.github.com>
2020-03-23 15:13:08 -0400

    Update WiFiDataTransfer.ino

--- WiFiDataTransfer.ino
+++ WiFiDataTransfer.ino
@@ -1,14 +1,23 @@
 /*
-Wifi Data Transfer initial test script, based off of Arduinos example file "WiFiWebServer" program which is modified for our team needs.
-Initially written by Abe Wickstrom on 2/20/2020
+A large part of this program (specifically the Arduino WiFi connection portion) is taken directly from Arduinos example file "WiFiWebServer" program.
+The parts of the program modified for the power monitor project is sectioned off, the rest is taken directly from the example file.
+Original program information:
+ created 13 July 2010
+ by dlf (Metodo2 srl)
+ modified 31 May 2012
+ by Tom Igoe
+
+Senior Design Project Smart Home Appliance Power Monitor
+Initially modified by Abe Wickstrom on 2/20/2020
+Last Modified 3/23/2020 by Abe Wickstrom
  */
 
 #include <SPI.h>
 #include <WiFiNINA.h>
 
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+//Wifi information input 
 char ssid[] = "Batcave1";        // your network SSID (name)
-char pass[] = "Flora2324!";    // your network password
+char pass[] = "PASSWORD";    // your network password
 
 int status = WL_IDLE_STATUS;
 
@@ -68,30 +77,48 @@ void loop() {
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
           client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 120");  // refresh the page automatically every 5 sec
+          client.println("Refresh: 120");  // refresh the page automatically every 120 sec
           client.println();
           client.println("<!DOCTYPE HTML>");
           client.println("<html>");
-          // output the value of each analog input pin
+          
+        //************************************************************************
+        //Power monitor code modification starts here:
+        //************************************************************************
         float sensorReadingAvg = 0;
         int sensorReadingSum = 0;
-        for (int test = 1; test < 111; test++) {
+        //Take 100 readings from the analog input pins, and compute the Average reading
+        for (int test = 1; test < 21; test++) { 
           int analogChannel = 0;
           int sensorReading = analogRead(analogChannel);
           sensorReadingSum = sensorReadingSum + sensorReading;
           sensorReadingAvg = sensorReadingSum/test;
-          client.print("analog input ");
+          float voltage = sensorReading*(3.3/1023);
+          float current = voltage / 6200; // For testing purposes, a 6200 Ohm resistor is being used
+          client.print("Channel Num: ");
           client.print(analogChannel);
-          client.print(" is ");
-          client.print(sensorReading);
+          client.print(", Voltage: ");
+          client.print(voltage,3);
+          client.print(" V, Current: ");
+          client.print(current*1000,3);
+          client.print(" mA ");
           client.println("<br />");
         }
-          client.print("analog input average: ");
-          client.print(sensorReadingAvg);
+          float voltAvg = sensorReadingAvg*(3.3/1023);
+          float currentAvg = (voltAvg/6200)*1000;
+          
+          client.print("Voltage Average: ");
+          client.print(voltAvg,3); 
+          client.print("V, Current Average: ");
+          client.print(currentAvg,3); //same current calculation as above
+          client.print(" mA ");
           client.println("<br />");
           client.println("</html>");
           break;
         }
+        //************************************************************************
+        //power monitor code modification end here
+        //************************************************************************
         if (c == '\n') {
           // you're starting a new line
           currentLineIsBlank = true;
